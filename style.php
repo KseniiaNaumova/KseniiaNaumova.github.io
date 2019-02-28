b60717ea7675f8c7ee5dc17cc7a793a7b24955f7
KseniiaNaumova <48092929+KseniiaNaumova@users.noreply.github.com>
2019-02-28 17:04:35 +0300

    Update style.php

--- style.php
+++ style.php
@@ -6,8 +6,8 @@
   $subject = "Регистрация на сайте url_вашего_сайта";
   $msg = "Ваши данные формы регистрации:\n" ."Имя: $name\n" ."Пол: $sex\n" ."Ваш email: $email\n" ."Страна: $country";
   $headers = "Content-type: text/plain; charset=UTF-8" . "\r\n";
-  $headers .= "From: Ваше_имя <ваш_email>" . "\r\n";
-  $headers .= "Bcc: ваш_email". "\r\n";
+  $headers .= "From: Helnika ksu0704@ukr.net" . "\r\n";
+  $headers .= "Bcc: ksu0704@ukr.net "\r\n";
   if(!empty($name) && !empty($sex) && !empty($email) && !empty($country) && filter_var($email, FILTER_VALIDATE_EMAIL)){
     mail($email, $subject, $msg, $headers);
     echo "Спасибо! Вы успешно зарегистрировались.";
